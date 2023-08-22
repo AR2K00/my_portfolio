93c749946bfa7809a7b90b15bf2362306bc318a0
AR2K00 <50752407+AR2K00@users.noreply.github.com>
2023-08-22 18:30:11 -0400

    basic home - start

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'projects/index'
+  get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root 'home#index'
+  get 'projects', to: 'projects#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
